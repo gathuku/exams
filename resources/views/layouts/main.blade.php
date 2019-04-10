a97edf3a58abf9c65d2fbfbbaf84d67539a521ec
moses gathuku <mosesgathuku95@gmail.com>
2019-04-10 13:56:52 +0300

    :art: add links icon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -68,8 +68,8 @@
               <li><a class="app-menu__item" href="/unit"><i class="app-menu__icon fa fa-calendar-minus-o"></i><span class="app-menu__label">Units</span></a></li>
               <li><a class="app-menu__item" href="/mark"><i class="app-menu__icon fa fa-id-card"></i><span class="app-menu__label">Exam marks</span></a></li>
               <li><a class="app-menu__item" href="/transcript"><i class="app-menu__icon fa fa-envelope"></i><span class="app-menu__label">Transcripts</span></a></li>
-              <li><a class="app-menu__item" href="/registerunit"><i class="app-menu__icon fa fa-envelope"></i><span class="app-menu__label">Register units</span></a></li>
-              <li><a class="app-menu__item" href="/retakes"><i class="app-menu__icon fa fa-envelope"></i><span class="app-menu__label">Retakes</span></a></li>
+              <li><a class="app-menu__item" href="/registerunit"><i class="app-menu__icon fa fa-sitemap"></i><span class="app-menu__label">Register units</span></a></li>
+              <li><a class="app-menu__item" href="/retakes"><i class="app-menu__icon fa fa-file-pdf-o"></i><span class="app-menu__label">Retakes</span></a></li>
 
       </ul>
     </aside>
