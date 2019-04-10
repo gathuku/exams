27da6e040979574c5f115eb1b32b1539343d059a
moses gathuku <mosesgathuku95@gmail.com>
2019-04-10 16:18:04 +0300

    :art: add rights to main menu links

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -30,7 +30,7 @@
         <!-- User Menu-->
         <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i></a>
           <ul class="dropdown-menu settings-menu dropdown-menu-right">
-            <li><a class="dropdown-item" href="page-user.html"><i class="fa fa-cog fa-lg"></i> Settings</a></li>
+            
             <li><a class="dropdown-item" href="/user/{{Auth()->user()->id}}"><i class="fa fa-user fa-lg"></i> Profile</a></li>
             <li><a class="dropdown-item" href="{{route('logout')}}"
              onclick="event.preventDefault();
@@ -61,16 +61,23 @@
       </div>
       <ul class="app-menu">
         <li><a class="app-menu__item active" href="{{route('home')}}"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+             @admin
+             <li><a class="app-menu__item" href="/user"><i class="app-menu__icon fa fa-user-circle-o"></i><span class="app-menu__label">Students</span></a></li>
+             <li><a class="app-menu__item" href="/semester"><i class="app-menu__icon fa fa-clone"></i><span class="app-menu__label">Semesters</span></a></li>
+             <li><a class="app-menu__item" href="/exam"><i class="app-menu__icon fa fa-book"></i><span class="app-menu__label">Exams</span></a></li>
 
-        <li><a class="app-menu__item" href="/user"><i class="app-menu__icon fa fa-user-circle-o"></i><span class="app-menu__label">Students</span></a></li>
-              <li><a class="app-menu__item" href="/semester"><i class="app-menu__icon fa fa-clone"></i><span class="app-menu__label">Semesters</span></a></li>
-              <li><a class="app-menu__item" href="/exam"><i class="app-menu__icon fa fa-book"></i><span class="app-menu__label">Exams</span></a></li>
-              <li><a class="app-menu__item" href="/unit"><i class="app-menu__icon fa fa-calendar-minus-o"></i><span class="app-menu__label">Units</span></a></li>
-              <li><a class="app-menu__item" href="/mark"><i class="app-menu__icon fa fa-id-card"></i><span class="app-menu__label">Exam marks</span></a></li>
-              <li><a class="app-menu__item" href="/transcript"><i class="app-menu__icon fa fa-file-pdf-o"></i><span class="app-menu__label">Transcripts</span></a></li>
+             <li><a class="app-menu__item" href="/unit"><i class="app-menu__icon fa fa-calendar-minus-o"></i><span class="app-menu__label">Units</span></a></li>
+             <li><a class="app-menu__item" href="/mark"><i class="app-menu__icon fa fa-id-card"></i><span class="app-menu__label">Exam marks</span></a></li>
+
+              @endadmin
+              @student
               <li><a class="app-menu__item" href="/registerunit"><i class="app-menu__icon fa fa-sitemap"></i><span class="app-menu__label">Register units</span></a></li>
+              @endstudent
+              <li><a class="app-menu__item" href="/transcript"><i class="app-menu__icon fa fa-file-pdf-o"></i><span class="app-menu__label">Transcripts</span></a></li>
               <li><a class="app-menu__item" href="/retakes"><i class="app-menu__icon fa fa-envelope"></i><span class="app-menu__label">Retakes</span></a></li>
-
+             @admin
+              <li><a class="app-menu__item" href="/transcript"><i class="app-menu__icon fa fa-cog"></i><span class="app-menu__label">Settings</span></a></li>
+             @endadmin
       </ul>
     </aside>
     @yield('content')
