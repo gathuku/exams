6051d5d4fd653f3e772a5cac1c380c58797d7423
moses gathuku <mosesgathuku95@gmail.com>
2019-04-12 14:46:51 +0300

    add session seed

---
+++ database/seeds/SessionTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SessionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('session')->delete();
+        DB::table('session')->insert([
+          0 =>[
+            'id' => 1,
+            'regno' => 'IN14/20358/15',
+            'year_id'=>1,
+            'semester_id'=>1,
+          ],
+        ]);
+    }
+}
